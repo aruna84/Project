30b9af7467ed923c9b4dd228df332d4a50358006
aruna84 <arunashannu@yahoo.com>
2025-04-24 23:38:35 -0700

    add new

--- Example.java
+++ Example.java
@@ -2,7 +2,7 @@
 class Example{
 	public static void main(String[]args){
 		
-		System.out.println("You born on wehre : ");
-		System.out.println("You born on wehre who : ");
+		System.out.println("name : ");
+		
 	}
 }
--- Sample.java
+++ Sample.java
@@ -2,8 +2,8 @@
 class Sample{
 	public static void main(String[]args){
 		
-		System.out.println("You born on wehre : ");
-		System.out.println("You born on wehre who : ");
+		System.out.println("112You born on wehre : ");
+		
 	}
 }
 
